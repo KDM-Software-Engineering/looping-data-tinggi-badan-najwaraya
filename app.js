6c6338c8f8324e8886887ffed56ba0d47bc45784
najwaraya <najwaraya@gmail.com>
2024-10-24 15:11:47 +0700

    selesai

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
-function runner() {
+// function runner() {
 
   // TULIS SCRIPT KAMU DI SINI
 
 
+let tinggiBadan =[" 150", "155", "148", "165", "170", "168", "166"]
+let namaAtlet = ["Rizky", "Ambar", "Rada", "Tika", "Malik", "Nanang", "Azizah"]
+for (let i = 0; i < 7 ; i++) {
 
 
-
-
-  /// JANGAN UBAH SELAINNYA
+  console.log(`Atlet ${namaAtlet[i]}  tinggi badanya ${tinggiBadan [i]} cm`)
 }
+  /// JANGAN UBAH SELAINNYA
+// }
 
-module.exports = runner
+// module.exports = runner
